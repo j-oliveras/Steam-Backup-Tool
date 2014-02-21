a568e9555b291e24e4df9cee6f04d1db71dca5cb
Juergen Tem <j.tem@gmx.de>
2014-02-21 12:13:54 +0100

    using C++ 7z wrapper for faster compression
    see http://bitbucket.org/UniqProject/sevenzip-lib
    wrapper is used only for lzma2 compression, as it is not fully tested yet using multiple instances

--- src/Main/Tasks/Backup/BackupJob.cs
+++ src/Main/Tasks/Backup/BackupJob.cs
@@ -12,6 +12,7 @@ namespace steamBackup
     class BackupJob : Job
     {
         private SevenZipCompressor sevenZip = null;
+        private SevenZipWrapper wrapper = null;
 
         public BackupJob()
         {
@@ -57,18 +58,43 @@ namespace steamBackup
                 fileList = Directory.GetFiles(dirSteam, "*.*", SearchOption.AllDirectories);
             }
 
-            if (sevenZip.CompressionMethod == CompressionMethod.Lzma2 && Utilities.getSevenZipRelease() > 64)
-            {
-                sevenZip.CustomParameters.Add("mt", Settings.lzma2Threads.ToString(CultureInfo.InvariantCulture));
-            }
-
-            sevenZip.Compressing += new EventHandler<ProgressEventArgs>(working);
-            sevenZip.FileCompressionStarted += new EventHandler<FileNameEventArgs>(started);
-            sevenZip.CompressionFinished += new EventHandler<EventArgs>(finished);
-            
             try
             {
-                sevenZip.CompressFiles(dirBackup,Utilities.upDirLvl(dirSteam).Length, fileList);
+                if (sevenZip.CompressionMethod == CompressionMethod.Lzma2)
+                {
+                    wrapper = new SevenZipWrapper(@"rsc\7z.dll", dirBackup);
+                    wrapper.Compressing += new EventHandler<ProgressEventArgs>(working);
+                    wrapper.FileCompressionStarted += new EventHandler<FileNameEventArgs>(started);
+                    wrapper.CompressionFinished += new EventHandler<EventArgs>(finished);
+                    wrapper.UseLzma2Compression = true;
+                    int compLevel = (int) sevenZip.CompressionLevel;
+                    switch (compLevel)
+                    {
+                        case 2:
+                            compLevel = 3;
+                            break;
+                        case 3:
+                            compLevel = 5;
+                            break;
+                        case 4:
+                            compLevel = 7;
+                            break;
+                        case 5:
+                            compLevel = 9;
+                            break;
+                    }
+                    wrapper.CompressionLevel = compLevel;
+                    wrapper.UseMultithreading = true;
+                    wrapper.MultithreadingNumThreads = Settings.lzma2Threads;
+                    wrapper.CompressFiles(Utilities.upDirLvl(dirSteam), fileList);
+                }
+                else
+                {
+                    sevenZip.Compressing += new EventHandler<ProgressEventArgs>(working);
+                    sevenZip.FileCompressionStarted += new EventHandler<FileNameEventArgs>(started);
+                    sevenZip.CompressionFinished += new EventHandler<EventArgs>(finished);
+                    sevenZip.CompressFiles(dirBackup, Utilities.upDirLvl(dirSteam).Length, fileList);
+                }  
             }
             catch (System.Exception e)
             {
